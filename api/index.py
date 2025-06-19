d7d7ca15851f3e257ee5767ba206838e0a681a50
nat-lima <naty0182@gmail.com>
2025-06-19 11:43:33 -0300

    adjustments

---
+++ api/index.py
@@ -0,0 +1,63 @@
+from flask import Flask, jsonify, json
+from pathlib import Path
+from os.path import join
+
+# Criando um dado de teste
+livros = [
+  {
+    "id": 0,
+    "titulo": "1984",
+    "autor": "George Orwell",
+    "ano_publicao": "1949"
+  },
+  {
+    "id": 1,
+    "titulo": "Laranja Mecânica",
+    "autor": "Anthony Burgess",
+    "ano_publicao": "1962"
+  },
+  {
+    "id": 2,
+    "titulo": "Admirável Mundo Novo",
+    "autor": "Aldous Huxley",
+    "ano_publicao": "1932"  
+  }
+]
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET'])
+def home():
+    return '<h1>Página da rota Home</h1>'
+
+@app.route('/sobre', methods=['GET'])
+def about():
+    return '<h1>Pagina da rota Sobre</h1>'
+
+@app.route('/portfolio', methods=['GET'])
+def portfolio():
+    return '<h1>Página da rota Portfolio</h1>'
+
+@app.route('/contato', methods=['GET'])
+def contact():
+    return '<h1>Página da rota Contato</h1>'
+
+# Puxando dados de data.json e serializando
+@app.route("/api/cervejas", methods=['GET'])
+def api():
+    current_dir = Path(__file__).parent
+    parent_dir = current_dir.parent
+    # File path
+    file_path = parent_dir / "data" / "data.json"
+    
+    with open(file_path, 'r') as file:
+        data = json.load(file)
+        return jsonify(data)
+
+# Serializando nosso dado de teste
+@app.route('/api/livros', methods=['GET'])
+def api_all():
+    return jsonify(livros)
+
+if __name__ == '__main__':
+    app.run()
